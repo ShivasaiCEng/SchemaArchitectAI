9950acc111351a30223db7343670fe966078136f
ShivasaiCEng <isha4shiva@gmail.com>
2025-12-16 14:41:40 +0530

    added the mongodb error reader

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,6 +5,8 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    console.log("ENV CHECK → MONGO_URI:", process.env.MONGO_URI);
+
     console.log(`   Database: ${conn.connection.name}`);
     return conn;
   } catch (error) {
